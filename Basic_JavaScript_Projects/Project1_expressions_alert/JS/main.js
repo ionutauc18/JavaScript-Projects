339ebd46bffdd516253e696169c91ecba109762c
ionutauc18 <108615889+ionutauc18@users.noreply.github.com>
2022-11-05 02:23:04 +0000

    Assignment Submission #1
    Submmiting Assignment #1 which includes multiple assignments learned during the first module

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello, Instructors!"); //Making an alert using the
+                                     // window.alert method
+document.write("Welcome to Assinment #1 of JavaScript Course."); //Displaing a text using
+                                                                 //the document.write method
+document.write("\" Today we gonna learn the sum of ," +
+ " 5 added to 5.\"" + " I\'m excited! "); //Demonstrating how to concatenate two variables
+
+var x = "10" //Giving a value to a variable
+
+var x = x.fontcolor("red"); //Using the font.color method
+                            //on x variable 
+
+document.write(x); //Writing the x variable with "red" fontcolor
+
+document.write(5 + 5); //Demonstrating an expression, 
+                       //keeping it in the original text
+
+
