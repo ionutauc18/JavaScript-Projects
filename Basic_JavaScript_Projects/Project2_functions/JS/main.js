1f83d5a95f628e3927ec411a70bff44872978dc7
ionutauc18 <108615889+ionutauc18@users.noreply.github.com>
2022-11-05 08:10:50 +0000

    Submmiting Assignment #2
    Adding Assignment #2 to my JavaScript-Projects repository

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { //Adding a function
+    var sentence = "I am from"; //Giving a variable to sentence  
+    sentence += " Scornicesti, a city from Romania!"; //Concatenating a string using +=operator
+    document.getElementById("demo").innerHTML = sentence; //Modifying the content of the HTMLelement
+}
