3870ce3b8a6269bd2ba85348a396247e00188341
ionutauc18 <108615889+ionutauc18@users.noreply.github.com>
2023-01-06 19:50:35 +0000

    Add files via upload

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+window.alert("Hello, Instructors!"); //Making an alert using the
+                                     // window.alert method
+document.write("Welcome to Assignment #1 of JavaScript Course."); //Displaing a text using
+                                                                 //the document.write method
+
+let artist = "Little Mix"; 
+var z = ('My favorite artists is the group ' + artist + '.');
+
+var z = z.fontcolor("red"); //Using the font.color method
+                            //on z variable 
+
+document.write(z); //Writing the z variable with "red" fontcolor
+
+document.write(5 + 5); //Demonstrating an expression, 
+                       //keeping it in the original text
+
+
